2fe675b86add253b13112b834c52b3b5171df7fa
RAIHAN SIKDER <sikderraihan693@gmail.com>
2025-06-20 00:28:55 +0600

    Update ContestAdapter.java

--- app/src/main/java/com/example/codeforcesreminder/ContestAdapter.java
+++ app/src/main/java/com/example/codeforcesreminder/ContestAdapter.java
@@ -4,46 +4,42 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
 import java.text.SimpleDateFormat;
 import java.util.List;
 import java.util.Locale;
 
 public class ContestAdapter extends RecyclerView.Adapter<ContestAdapter.ViewHolder> {
-    private List<Contest> contests;
-    private OnContestClickListener clickListener;
-    private OnScheduleReminderListener reminderListener;
+    private final List<Contest> contests;
+    private final OnContestClickListener clickListener;
 
     public interface OnContestClickListener {
         void onContestClick(Contest contest);
     }
 
-    public interface OnScheduleReminderListener {
-        void onScheduleReminder(Contest contest, long reminderTime);
-    }
-
-    public ContestAdapter(List<Contest> contests, OnContestClickListener clickListener, OnScheduleReminderListener reminderListener) {
+    public ContestAdapter(List<Contest> contests, OnContestClickListener clickListener) {
         this.contests = contests;
         this.clickListener = clickListener;
-        this.reminderListener = reminderListener;
     }
 
     @NonNull
     @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_contest, parent, false);
-        return new ViewHolder(view);
+    public ContestAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_contest, parent, false);
+        return new ViewHolder(v);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull ContestAdapter.ViewHolder holder, int position) {
         Contest contest = contests.get(position);
         holder.contestName.setText(contest.getName());
         SimpleDateFormat sdf = new SimpleDateFormat("dd MMM yyyy, HH:mm", Locale.getDefault());
         holder.contestTime.setText(sdf.format(contest.getStartTime()));
+
         holder.itemView.setOnClickListener(v -> clickListener.onContestClick(contest));
-        // Reminder buttons removed
     }
 
     @Override
@@ -54,7 +50,7 @@ public class ContestAdapter extends RecyclerView.Adapter<ContestAdapter.ViewHold
     static class ViewHolder extends RecyclerView.ViewHolder {
         TextView contestName, contestTime;
 
-        ViewHolder(View itemView) {
+        ViewHolder(@NonNull View itemView) {
             super(itemView);
             contestName = itemView.findViewById(R.id.contestName);
             contestTime = itemView.findViewById(R.id.contestTime);
